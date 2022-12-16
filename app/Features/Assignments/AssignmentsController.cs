4a5db9a984dfa6635e8c90ec867f906d5f8598ec
Robert Gherasim <ghera.robi@gmail.com>
2022-12-16 10:21:46 -0800

    added Get and Post for basic model layout

---
+++ app/Features/Assignments/AssignmentsController.cs
@@ -0,0 +1,69 @@
+﻿using app.Features.Assignments.Models;
+using app.Features.Assignments.Views;
+using Microsoft.AspNetCore.Mvc;
+
+namespace app.Features.Assignments;
+
+[ApiController]
+[Route("assignments")]
+public class AssignmentsController
+{
+    private static List<AssignmentModel> _mockDb = new List<AssignmentModel>();
+
+    [HttpPost]
+    public AssignmentResponse Add(AssignmentRequest request)
+    {
+        var assignment = new AssignmentModel//mapping
+        {
+            Id = Guid.NewGuid().ToString(),
+            Created = DateTime.UtcNow,
+            Updated = DateTime.UtcNow,
+            Subject = request.Subject,
+            Description = request.Description,
+            Deadline = request.Deadline
+        };
+        
+        _mockDb.Add(assignment);
+
+        return new AssignmentResponse
+        {
+            Id = assignment.Id,
+            Subject = assignment.Subject,
+            Description = assignment.Description,
+            Deadline = assignment.Deadline
+        };
+    }
+
+    [HttpGet]
+    public IEnumerable<AssignmentResponse> Get()
+    {
+        return _mockDb.Select(
+            assignment => new AssignmentResponse()
+            {
+                Id = assignment.Id,
+                Subject = assignment.Subject,
+                Description = assignment.Description,
+                Deadline = assignment.Deadline      
+            }
+        ).ToList();
+    }
+
+    [HttpGet("{id}")]
+    public AssignmentResponse Get([FromRoute] string id)
+    {
+        var assignment = _mockDb.FirstOrDefault(x => x.Id == id);
+        if (assignment is null)
+        {
+            return null;
+        }
+        
+        return new AssignmentResponse
+        {
+            Id = assignment.Id,
+            Subject = assignment.Subject,
+            Description = assignment.Description,
+            Deadline = assignment.Deadline
+        };
+    }
+    //TODO HttpDeLETE, HttpPatch, delete dupa id si update cu return la obiect sub forma de AssignmentResponse
+}
